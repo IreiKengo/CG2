52f4da16d23911369b31035e30ffc35446e117c8
IreiKengo <k024g0087@g.neec.ac.jp>
2025-04-16 15:48:09 +0900

    Logの実装ができた

--- main.cpp
+++ main.cpp
@@ -1,13 +1,50 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
 
 
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
 
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-	WPARAM wparam, LPARAM lparam) {
+	WPARAM wparam, LPARAM lparam)
+{
 	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
+	switch (msg)
+	{
 		//ウィンドウが破棄された
 	case WM_DESTROY:
 		//OSに対して。アプリの終了えを伝える
@@ -21,7 +58,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 
 
 //Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
 
 	WNDCLASS wc{};
 	//ウィンドウプロシージャ
@@ -63,12 +101,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
 		nullptr);
 	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
+	Log("Hello DirectX!\n");
+	Log(
+		ConvertString(
+			std::format(
+				L"clientSize:{},{}\n",
+				kClientWidth,
+				kClientHeight
+			)
+		)
+	);
+
+
 
 	MSG msg{};
 	//ウィンドウの×ボタンが押されるまでループ
-	while (msg.message != WM_QUIT) {
+	while (msg.message != WM_QUIT)
+	{
 		//Windowにメッセージが来てたら最優先で処理させる
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		} else {
@@ -76,7 +128,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
 		}
 	}
 
-	OutputDebugStringA("Hello DirectX!\n");
+
 
 
 
